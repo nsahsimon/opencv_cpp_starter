359407f3d56778e1913b3d3aedcf90791536f080
nsahsimon <nsahsimonai01@gmail.com>
2023-08-10 07:49:07 +0100

    second commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/SMARTECH/Desktop/ME/opencv_c++_projects/opencv_cpp_starter
-# Build directory: C:/Users/SMARTECH/Desktop/ME/opencv_c++_projects/opencv_cpp_starter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
